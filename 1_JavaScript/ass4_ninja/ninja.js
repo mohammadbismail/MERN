97942fb8b9684923b5def2436dedf1f34d728617
mohammadbismail <m.basim.ismail@gmail.com>
2022-12-04 22:00:14 +0200

    super ninja assignment

--- 1_JavaScript/ass4_ninja/ninja.js
+++ 1_JavaScript/ass4_ninja/ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
-  constructor(myName) {
+  constructor(myName,myHealth=0,mySpeed=3,myStrength=3) {
     this.name = myName;
-    this.health = 0;
-    this.speed = 3;
-    this.strength = 3;
+    this.health = myHealth;
+    this.speed = mySpeed;
+    this.strength = myStrength;
   }
   sayName() {
     console.log(this.name);
@@ -21,12 +21,26 @@ class Ninja {
     );
   }
   drinkSake() {
-    this.health = +10;
+    this.health += 10;
   }
 }
 
-const ninja1 = new Ninja("Ninja1");
-ninja1.sayName();
-ninja1.showStatus();
-ninja1.drinkSake();
-ninja1.showStatus();
+class Sensi extends Ninja {
+    constructor(myName){
+        super(myName,200,10,10);
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// const ninja1 = new Ninja("Ninja1");
+// ninja1.sayName();
+// ninja1.drinkSake();
+// ninja1.showStatus();
+
+const dojo1 = new Sensi("dojo1");
+dojo1.speakWisdom();
+dojo1.showStatus();
---
+++ 1_JavaScript/ass4_ninja/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const ninja1 = new Ninja("Ninja1");
+// ninja1.sayName();
+// ninja1.drinkSake();
+// ninja1.showStatus();
