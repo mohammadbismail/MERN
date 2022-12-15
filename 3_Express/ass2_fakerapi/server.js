c255d11b3db5809313d35f742d0d8e305e8cdd5e
mohammadbismail <m.basim.ismail@gmail.com>
2022-12-15 14:42:43 +0200

    faker

--- 3_Express/ass2_fakerapi/server.js
+++ 3_Express/ass2_fakerapi/server.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const app = express();
+const { faker } = require("@faker-js/faker");
 
-// req is short for request
-// res is short for response
-app.get("/api", (req, res) => {
-  res.send("Our express api server is now sending this over to the browser");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const createUser = () => {
+  return {
+    _id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    phoneNumner: faker.phone.number(),
+    password: faker.internet.password(),
+  };
+};
+const createCompany = () => {
+  return {
+    _id: faker.datatype.uuid(),
+    name: faker.company.name(),
+    address: {
+      street: faker.address.street(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+  };
+};
+app.get("/api/users/new", (req, res) => {
+  res.json(createUser());
+});
+app.get("/api/companies/new", (req, res) => {
+  res.json(createCompany());
+});
+app.get("/api/user/company", (req, res) => {
+  res.json({
+    user: createUser(),
+    company: createCompany(),
+  });
 });
 
 const server = app.listen(8000, () =>
